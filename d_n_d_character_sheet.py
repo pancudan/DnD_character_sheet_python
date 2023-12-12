90c774898b7f4997ca03e9de7286a4966fd6ec3b
pancudan <120690990+pancudan@users.noreply.github.com>
2023-12-12 21:01:55 +0200

    Update d_n_d_character_sheet.py

--- d_n_d_character_sheet.py
+++ d_n_d_character_sheet.py
@@ -3,6 +3,7 @@ import random
 races = ["Dwarf", "Elf", "Orc", "Halfling", "Human", "Dragonborn", "Gnome", "Half-Elf", "Half-Orc", "Tiefling"]
 classes = ["Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"]
 backgrounds = ["Acolyte", "Charlatan", "Criminal", "Entertainer", "Folk Hero", "Guild Artisan", "Hermit", "Noble", "Outlander", "Sage", "Sailor", "Soldier", "Urchin"]
+religions = ["Forgotten Realms", "Greyhawk", "Dragonlance", "Eberron", "Ravenloft", "Planescape", "Mystara"]
 
 def choose_option(options, message):
     print("\n".join([f"{i+1}. {option}" for i, option in enumerate(options)]))
@@ -11,8 +12,6 @@ def choose_option(options, message):
         print("Invalid choice. Please enter a valid number.")
         choice = input(message)
     return options[int(choice) - 1]
-
-
 def generate_ability_scores():
     return {
         "Strength": random.randint(8, 18),
@@ -22,27 +21,19 @@ def generate_ability_scores():
         "Wisdom": random.randint(8, 18),
         "Charisma": random.randint(8, 18)
     }
-
-
 def generate_character():
     character = {}
-
-
     character["Race"] = choose_option(races, "Choose a race (enter the corresponding number): ")
     character["Class"] = choose_option(classes, "Choose a class (enter the corresponding number): ")
     character["Background"] = choose_option(backgrounds, "Choose a background (enter the corresponding number): ")
-    
+    character["Religion"] = choose_option(religions, "Choose a religion (enter the corresponding number): ")
     character["Name"] = input("Enter your character's name: ")
     character.update(generate_ability_scores())
-
     return character
-
 def display_character_sheet(character):
     print("\nCharacter Sheet:")
     for key, value in character.items():
         print(f"{key}: {value}")
-
-print("Welcome to Dungeons and Dragons Character Sheet Creation!")
+print("Welcome to Dungeons and Dragons Character Creation!")
 new_character = generate_character()
-
 display_character_sheet(new_character)
